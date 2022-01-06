fab816acec87de5ef69dc513e0c9dd9cf6de4f6d
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2022-01-06 19:54:30 -0300

    Implementação dos botões 'voltar' e 'ver site' do InfoDialog

--- app/src/main/java/com/example/trabalho1unidade/fragments/InfoDigalogFragment.java
+++ app/src/main/java/com/example/trabalho1unidade/fragments/InfoDigalogFragment.java
@@ -1,7 +1,11 @@
 package com.example.trabalho1unidade.fragments;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,10 +16,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.trabalho1unidade.R;
 
-public class InfoDigalogFragment extends DialogFragment {
+public class InfoDigalogFragment extends DialogFragment implements DialogInterface.OnClickListener {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -24,8 +30,29 @@ public class InfoDigalogFragment extends DialogFragment {
         builder.setTitle("Desenvolvido por:");
         builder.setMessage("João Victtor Carvalho de Luna\nMariana\nMatheus de Andrade Silva\nPedro Henrique Alves Cardoso");
 
+        builder.setPositiveButton("Ver site", this);
+        builder.setNegativeButton("Voltar", this);
+
         return builder.create();
     }
 
 
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        Activity activity = getActivity();
+
+        if(which == -1){
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Carvs10/Mobile1Unid"));
+            startActivity(browserIntent);
+        }
+
+        if(activity instanceof OnClickOnInfo){
+            OnClickOnInfo listener = (OnClickOnInfo) activity;
+            listener.onClick(which);
+        }
+    }
+
+    public interface OnClickOnInfo{
+        public void onClick(int button);
+    }
 }
